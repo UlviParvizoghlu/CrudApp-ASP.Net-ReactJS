c77960bedb88d09f40610a3f104c3e173146fb02
Ulvi Spaethe <ulvispaethe@gmail.com>
2023-07-15 15:30:57 +0100

    Frontend Finish

--- frontend/src/CRUD.jsx
+++ frontend/src/CRUD.jsx
@@ -5,29 +5,11 @@ import Modal from 'react-bootstrap/Modal';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import axios from 'axios'
+import {ToastContainer,toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const CRUD = () => {
-  const empdata = [
-    {
-      id: 1,
-      name: "Ulvi",
-      age: 20,
-      isActive: 1,
-    },
-    {
-      id: 2,
-      name: "Tahir",
-      age: 19,
-      isActive: 1,
-    },
-    {
-      id: 3,
-      name: "Semistan",
-      age: 29,
-      isActive: 0,
-    },
-  ];
-
   const [data, setData] = useState([]);
   const [show, setShow] = useState(false);
 
@@ -44,25 +26,120 @@ const CRUD = () => {
   const handleShow = () => setShow(true);
 
   useEffect(() => {
-    setData(empdata);
+    getData()
   }, []);
+
+  const getData = () =>{
+    axios.get('http://localhost:5024/api/Employee').then((result) => {
+
+    setData(result.data)
+      
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
 
   const handleEdit = (id) =>{
     handleShow()
+    axios.get(`http://localhost:5024/api/Employee/${id}`).then((result) => {
+
+    setEditName(result.data.name)
+    setEditAge(result.data.age)
+    setEditIsActive(result.data.isActive)
+    setEditId(id)
+      
+    }).catch((error) => {
+      console.log(error);
+    });
   }
   const handleDelete = (id) =>{
-    if(window.confirm('Are you sure delete this?') == true)
+    if(window.confirm('Are you sure delete this?') === true)
     {
-      alert(id)
+      axios.delete(`http://localhost:5024/api/Employee/${id}`).then((result)=>{
+        if(result.status === 200)
+        {
+          toast.success('Employee has been deleted')
+          getData()
+        }
+      }).catch((error)=>{
+        toast.error(error)
+      })
     }
   }
 
   const handleUpdate = (id) =>{
+    const url = `http://localhost:5024/api/Employee/${editId}`
+    const data = {
+      "id" : editId,
+      "name" : editName,
+      "age" : editAge,
+      "isActive" : editIsActive
+      
+    }
+    axios.put(url,data)
+    .then((result) =>{
+      getData();
+      clear()
+      toast.success('Employee has been updated')
+    }).catch((error)=>{
+      toast.error(error)
+    })
+    handleClose()
    
+  }
+
+  const handleSave = () =>{
+    const url = 'http://localhost:5024/api/Employee'
+    const data = {
+      "name" : name,
+      "age" : age,
+      "isActive" : isActive
+    }
+
+    axios.post(url,data)
+    .then((result) =>{
+      getData();
+      clear()
+      toast.success('Employee has been added')
+    }).catch((error)=>{
+      toast.error(error)
+    })
+  }
+
+
+
+  const clear = () =>{
+    setName('');
+    setAge('');
+    setIsActive(0);
+    setEditName('');
+    setEditAge('');
+    setEditIsActive(0);
+    setEditId('');
+  }
+
+  const handleActiveChange = (e) =>{
+    if(e.target.checked)
+    {
+      setIsActive(1)
+    }
+    else{
+      setIsActive(0)
+    }
+  }
+  const handleEditActiveChange = (e) =>{
+    if(e.target.checked)
+    {
+      setEditIsActive(1)
+    }
+    else{
+      setEditIsActive(0)
+    }
   }
 
   return (
     <Fragment>
+      <ToastContainer/>
       <Container className="py-3">
       <Row>
         <Col>
@@ -72,11 +149,11 @@ const CRUD = () => {
         <input type="text" className="form-control" placeholder="Enter Age" value={age} onChange={(e)=> setAge(e.target.value)} />
         </Col>
         <Col>
-        <input type="checkbox" checked={isActive ===1 ? true : false} onChange={(e)=> setIsActive(e)} value={isActive}/>
+        <input type="checkbox" checked={isActive ===1 ? true : false} onChange={(e)=> handleActiveChange(e)} value={isActive}/>
         <label>isActive</label>
         </Col>
         <Col>
-        <button className="btn btn-primary">Submit</button>
+        <button className="btn btn-primary" onClick={()=> handleSave()}>Submit</button>
         </Col>
       </Row>
     </Container>
@@ -123,7 +200,7 @@ const CRUD = () => {
         <input type="text" className="form-control" placeholder="Enter Age" value={editAge} onChange={(e)=> setEditAge(e.target.value)} />
         </Col>
         <Col>
-        <input type="checkbox" checked={editIsActive ===1 ? true : false} onChange={(e)=> setEditIsActive(e)} value={editIsActive}/>
+        <input type="checkbox" checked={editIsActive ===1 ? true : false} onChange={(e)=> handleEditActiveChange(e)} value={editIsActive}/>
         <label>isActive</label>
         </Col>
       </Row>
