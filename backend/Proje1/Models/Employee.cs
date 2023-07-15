1a286b05cfae03ba82e9fea0634086710c087b93
Tahir9176 <tahirhsnli5@gmail.com>
2023-07-16 00:48:31 +0400

    BackEnd

---
+++ backend/Proje1/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Proje1.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Age { get; set; }
+    public int IsActive { get; set; }
+}
---
+++ backend/Proje1/Models/EmployeeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Proje1.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; } 
+    }
+}
